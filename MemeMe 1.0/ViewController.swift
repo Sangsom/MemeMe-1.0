fd77ba7a4217021549b84da867e6f7bd7dc919cb
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-05-30 12:31:11 +0300

    Initial Commit

---
+++ MemeMe 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Rinalds Domanovs on 30/05/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
